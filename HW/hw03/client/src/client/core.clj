c3b20cf01bef06a96e09a01314c7fde94a45f9b0
Jesse Nelson <jnels124@msudenver.edu>
2014-02-22 17:54:01 -0700

    Updating to display submitted solution

--- HW/hw03/client/src/client/core.clj
+++ HW/hw03/client/src/client/core.clj
@@ -1,6 +1,95 @@
-(ns client.core)
+(ns client.core
+  (import java.net.Socket
+          (java.io BufferedReader PrintWriter InputStreamReader))
+  (use clojure.java.io)
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def port 4567)
+
+(defn kill-resources
+  [ins outs socket excode]
+  (.close ins)
+  (.close outs)
+  (.close socket)
+  (System/exit excode))
+
+
+(defn process-header
+  "Handles the header from HTTP response and will determine the RTT of request"
+  [ln strm]
+  (when-not (or (= ln "") (= ln nil))
+    (println ln)
+    (process-header (.readLine strm) strm))
+  strm)
+
+(defn process-body
+  [strm out-file]
+  ;;Now process the body
+  ;;Have to write to path directly due to jar app
+  (let [wrtr  (str  (System/getProperty "user.dir")  "/" (cond (= out-file "") "default.html"
+                                              :else out-file))]
+    ;Creates file (in current directory) to write request body to,
+    ;or erases the contents of the file if already there
+    (spit wrtr "")
+    (loop [ctr 0
+           ln (.readLine strm)]
+      (spit wrtr (str ln "\n") :append true) ;;Write the line to outputfile
+      (if (and (not= ctr 4)
+               (not= ln nil)) ;; Prevent infinite loop in case server terminates.
+        (if (= "" ln)
+          (recur (inc ctr) (.readLine strm))
+          (recur 0 (.readLine strm))))))) ;; Done processing body.
+
+(defn message-and-response
+  [message]
+  (println message)
+  (read-line))
+
+(defn gen-http-request
+  "This method formats user request as an http request"
+  [[srvr-addr type req-file http-version user-agent]]
+  (let [message
+        (str type " /" req-file " HTTP/" http-version "\n"
+             "Host: " srvr-addr "\n"
+             "User Agent: " user-agent "\r\n")]
+
+    [message req-file]))
+
+(defn gen-user-request
+  [srvr-addr]
+  (let [mthd-type (message-and-response "Enter the http method you would like to perform.")
+        requested-file (message-and-response "Please enter the name of the html file.")
+        http-version (message-and-response "What http version do you want to use for the request?")
+        user-agent (message-and-response "What user agent are you using?")]
+
+    [srvr-addr mthd-type requested-file http-version user-agent]))
+
+(defn handle-connection
+  [[msg req-file] instrm outstrm srv-addr socket]
+  (-> (time ((fn [] (.println outstrm msg) (try (.readLine instrm)
+                                               (catch Exception e (println "Socket closed by server"))))));; Times the write to out and read to in
+      (process-header instrm)
+      (process-body req-file))
+  (if (= "no" (message-and-response "Would you like to make another request? (yes/no)"))
+    (kill-resources instrm outstrm socket 0) ;; The stray 0 Signifies normal exit from application
+    (-> (gen-user-request srv-addr)
+            gen-http-request
+            (handle-connection instrm outstrm srv-addr socket))))
+
+(defn -main
+  [& args]
+  (let [srv-addr (message-and-response "Enter the DNS or ip of the server.")
+        ;;Create the socket inside a try/catch and also calculate the time which will print to *out*
+        socket (try (time (Socket. srv-addr port))
+                    (catch Exception e
+                      (println "There was an error requesting socket from the server. Please restart."
+                      (.getMessage e)
+                      (System/exit -1))))]
+    (-> srv-addr
+        gen-user-request
+        gen-http-request
+        (handle-connection
+         (BufferedReader. (InputStreamReader. (.getInputStream socket)))
+         (PrintWriter. (.getOutputStream socket) true)
+         srv-addr
+         socket))))
--- HW/hw03/server/src/server/tcp_multi_thread.clj
+++ HW/hw03/server/src/server/tcp_multi_thread.clj
@@ -1,36 +1,86 @@
 (ns server.tcp-multi-thread
   (import (java.net ServerSocket Socket SocketException)
-          (java.io InputStreamReader OutputStreamWriter)))
+          (java.io InputStreamReader BufferedReader OutputStreamWriter PrintWriter)
+          (java.util Date Calendar)
+          (java.text DateFormat SimpleDateFormat)
+          (clojure.lang LineNumberingPushbackReader))
+  (use [clojure.string :only (join split)])
+  (require [clojure.java.io :as io])
+  (:gen-class))
+
 (def port 4567)
+(def hosted-files #{"/CS3700.html"})
+(def supported-methods #{"GET"})
+(def cal (Calendar/getInstance))
+(def date-fmt (SimpleDateFormat. "yyyy/MM/dd HH:mm:ss"))
+
 (defn run-thread
   "This method will take some function and start a thread with it.
    Note that all Clojure functions implement the Java runnable interface
    by default so fnctn is runnable."
   [fnctn]
+  (println "Starting new thread")
   (.start (Thread. fnctn)))
 
 (defn start-server
   "creates and returns a server socket on declared port and will pass the client
   socket to accept-client-socket on connection"
   [accept-client-socket]
-    (let [ss (new ServerSocket port)]
-      (run-thread #(when-not (.isClosed ss)
-                    (try (accept-client-socket (.accept ss ))
-                         (catch SocketException e
-                           (println "There was an error with the socket." (.getMessage e))))
-                    (recur)))
-      ss))
+  (let [ss (ServerSocket. port)] ;; Create instance of java.net.ServerSocket
+    (run-thread #(when-not (.isClosed ss) ;; Run a new thread to listen for client requests for socket
+                   (try (accept-client-socket (.accept ss)) ;;
+                        (println "accept-client-socket called")
+                        (catch SocketException e
+                          (.close ss)
+                          (println "There was an error with the socket." (.getMessage e))))
+                   ;;repeat until server socket is closed ..
+                   ;;This will not happen unless an exception
+                   ;;or user event triggers termination (ie C-c)
+                   (recur)))
+    ss))
+
+(defn process-header
+  "Process header lines and determine appropriate status line"
+  [[mthd-typ req-file vrsn]]
+  (cond
+   (= nil (some #{mthd-typ} supported-methods)) ["400 Bad Request" vrsn nil]
+   (= nil (some #{req-file} hosted-files)) ["404 Not Found" vrsn nil]
+   :else ["200 OK" vrsn req-file]))
+
+(defn gen-response
+  [[rspns-code vrsn req-file]]
+  (str vrsn " " rspns-code "\r\n"
+       "Date: " (.format date-fmt (.getTime cal)) "\r\n"
+       "Server: IMPERATIVE=JAVA=SUCKS! FUNCTIONAL=CLOJURE=AWESOME!\r\n"
+       "\r\n"
+       ;; append the file to response for 200 OK case
+       (if (= rspns-code "200 OK")
+         (str (-> (.substring req-file 1) io/resource io/file slurp))
+        "")
+       "\r\n\r\n\r\n\r\n")) ;; Append ending signature
 
 (defn process-request
-  "This will take the client socket input stream and write the request to the output stream"
-  [instrm outstrm]
-  (binding [*ns* (create-ns 'user) ;; For good measure ... Not sure how various threads react to ns
-            *warn-on-reflection* false ;; Avoid java type casiting warnings
-            *out* (OutputStreamWriter. outstrm)] ;; bind stndout to socket outstrm
-    (let [eof "\n\n"
-          rdr (InputStreamReader. instrm)]
-      (loop [nwln (read rdr false eof)]))
-    ))
+  "This will take the client socket input stream and write the request to the output stream
+   This function executes on a separate thread and continuously calls its self until client
+   closes sckt."
+  [instrm outstrm sckt]
+  (when-not (.isClosed sckt)
+    (try ;; Write response to clients input stream
+      (let [req-line (.readLine instrm)]
+        (println req-line)
+        (loop [line (.readLine instrm)]
+          (when-not (or (= "" line) (= nil line))
+            (println line)
+            (recur (.readLine instrm))))
+        ;; Print all reamining lines sent by client.
+         (.println outstrm (gen-response (process-header (split req-line #"\s")))))
+      (catch Exception e
+        (println "Stream disconnected by client")
+        (println  (.getMessage e))
+        (.close instrm)
+        (.close outstrm)
+        (.close sckt))))
+  (recur instrm outstrm sckt)) ;;repeat function
 
 (defn -main
   [& args]
@@ -39,6 +89,9 @@
      (fn socket-repl
        [sckt]
        (run-thread
-        #(process-request (.getInputStream sckt) (.getOutputStream sckt)))))
+        #(process-request
+          (BufferedReader. (InputStreamReader. (.getInputStream sckt)))
+          (PrintWriter. (.getOutputStream sckt) true)
+          sckt))))
     (catch Exception e
       (println (.printStackTrace e)))))
